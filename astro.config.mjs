2e02f3a7174d9263d94f9f61b28c4415ebf4a9da
damian234527 <92489768+damian234527@users.noreply.github.com>
2026-09-01 20:49:56 +0200

    Address

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const isCloudflare = process.env.CF_PAGES === '1';
-
 export default defineConfig({
   output: 'static',
-  site: isCloudflare ? 'plastart.damian-koscielny.workers.dev' : 'http://localhost:4321',
+  site: 'plastart.damian-koscielny.workers.dev',
+  // site: 'http://localhost:4321',
   // site: 'https://plastart.plast.one.pl',
   integrations: [sitemap()],
   i18n: {
