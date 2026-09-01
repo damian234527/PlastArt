6a619ed5388276eff87e18f5d03ee0e5b582f9d3
damian234527 <92489768+damian234527@users.noreply.github.com>
2026-09-01 21:04:17 +0200

    Address

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
-  site: 'plastart.damian-koscielny.workers.dev',
   // site: 'http://localhost:4321',
-  // site: 'https://plastart.plast.one.pl',
+  site: 'https://plastart.plast.one.pl',
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'pl',
