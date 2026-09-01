4dc550fb329d15b68f2813b498b4fee14bf7d963
damian234527 <92489768+damian234527@users.noreply.github.com>
2026-09-01 21:10:58 +0200

    Address

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+const siteUrl = process.env.CF_PAGES_URL || 'https://plastart.plast.one.pl';
 
 export default defineConfig({
   output: 'static',
-  site: 'plastart.pages.dev'
+  site: siteUrl,
   // site: 'http://localhost:4321',
   // site: 'https://plastart.plast.one.pl',
   integrations: [sitemap()],
